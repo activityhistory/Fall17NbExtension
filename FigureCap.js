b1cd01808ce8ed16b5a1340b86e42896560a6574
Regina <regina.cheng0616@gmail.com>
2017-11-07 10:20:39 -0800

    fixed console log

--- FigureCap.js
+++ FigureCap.js
@@ -5,21 +5,22 @@ require([
 	"base/js/events"
 ], 
 	function(Jupyter, events){
-		console.log("hello2");	
+			
 		events.on('output_appended.OutputArea', function(){
+			console.log('Output captured')
 			var cell1 = Jupyter.notebook.get_selected_cell();
 			cell_id = Jupyter.notebook.find_cell_index(cell1);
 			var cell2 = Jupyter.notebook.get_cell(cell_id-1);
 			try{
 				if(cell2.output_area.outputs["0"].data["image/png"]!==null){
-					console.log('meow')
+					console.log('Figure captured')
 				var newCell = Jupyter.notebook.insert_cell_above();
 				Jupyter.notebook.to_markdown(Jupyter.notebook.find_cell_index(newCell));
 				var newMcell = Jupyter.notebook.get_selected_cell()
 				newMcell.set_text("### Figure caption: ")
 				}
 				else{
-					console.log('hi')
+					console.log('there is no figure in the output')
 				}
 			}
 			catch(e){
